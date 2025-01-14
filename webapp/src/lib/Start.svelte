fc2ad0ab202dbb4339f5c888f5db6a22cca896f6
Roy C. Davies <roycdavies@gmail.com>
2025-01-15 12:40:56 +1300

    Tweaks

--- webapp/src/lib/Start.svelte
+++ webapp/src/lib/Start.svelte
@@ -10,12 +10,14 @@
     
 </script>
 <Container ui grid stackable style="padding-top:50px;padding-bottom:0px;vertical-align:top;">
-    <Row>
+    <Row style="padding-top:0px;">
         <Column seven wide>
             <Image ui src={imersialogo}/>
         </Column>
-        <Column nine wide justified style="border-top: 1px solid #4B6E8F; border-bottom: 1px solid #4B6E8F; padding-top: 20px; padding-bottom: 20px;">
+        <Column nine wide justified>
+            <div style="border-top: 0.5px solid rgb(100, 100, 100); margin-left: -3px; padding-top:20px;"></div>
             <Text ui large>Imersia is a trailblazer in geospatial gaming, launching at the dawn of cloud computing and the early days of smartphones. Its proprietary GeoSpatial Gaming Server remains unmatched in capability and innovation. This cutting-edge platform is available for licensing and use. Explore this site to learn more.</Text>
+            <div style="border-bottom: 0.5px solid rgb(100, 100, 100); margin-left: -3px; padding-bottom:20px;"></div>
         </Column>
     </Row>
     <Row>
--- webapp/src/lib/Strategy.svelte
+++ webapp/src/lib/Strategy.svelte
@@ -20,57 +20,71 @@
     <Column sixteen wide justified>
         <Image ui src={strategy}/>
     </Column>
+
     <Column five wide centered>
         <Text ui big>
             mixed reality
-        </Text>
-        <Divider ui/>
-        <Text ui large>
-            Leverage the Imersia multi-user, Mixed Reality engine to synchronise experiences in a place, thus engaging many people at the same time through their smart phones.
+            <Divider ui/>
         </Text>
     </Column>
     <Column six wide centered>
         <Text ui big>
             contextual intelligence
-        </Text>
-        <Divider ui/>
-        <Text ui large>
-            Imersia’s Mixed Reality platform adapts to people on the move, tailoring experiences to their time and place. By reaching users at the right moment, your brand can inspire more positive decisions.
+            <Divider ui/>
         </Text>
     </Column>
     <Column five wide centered>
         <Text ui big>
             realtime analytics
+            <Divider ui/>
         </Text>
-        <Divider ui/>
+    </Column>
+    <Column five wide justified>
         <Text ui large>
+            Leverage the Imersia multi-user, Mixed Reality engine to synchronise experiences in a place, thus engaging many people at the same time through their smart phones.
+        </Text>
+    </Column>
+    <Column six wide justified>
+        <Text ui large >
+            Imersia’s Mixed Reality platform adapts to people on the move, tailoring experiences to their time and place. By reaching users at the right moment, your brand can inspire more positive decisions.
+        </Text>
+    </Column>
+    <Column five wide justified>
+        <Text ui large >
             Success comes from understanding reactions. Imersia’s real-time engine adjusts experiences instantly, maximizing impact in the moment.
         </Text>
     </Column>
+
     <Column five wide centered>
         <Text ui big>
             design and development
-        </Text>
-        <Divider ui/>
-        <Text ui large>
-            Navigating a new medium is tough. With Imersia, your Mixed Reality experts guide ideation, explore possibilities, and focus on connecting with your customers effectively.
-        </Text>
+            <Divider ui/>
+       </Text>
     </Column>
     <Column six wide centered>
         <Text ui big>
             ideation
-        </Text>
-        <Divider ui/>
-        <Text ui large>
-            Understanding the potential of a new medium can be challenging. With Imersia, your Mixed Reality experts can engage early, guiding ideation, exploring possibilities, and focusing on the best ways to connect with your customers.
-        </Text>
+            <Divider ui/>
+       </Text>
     </Column>
     <Column five wide centered>
         <Text ui big>
             mobile digital strategy
+            <Divider ui/>
         </Text>
-        <Divider ui/>
-        <Text ui large>
+    </Column>
+    <Column five wide justified>
+        <Text ui large >
+            Navigating a new medium is tough. With Imersia, your Mixed Reality experts guide ideation, explore possibilities, and focus on connecting with your customers effectively.
+        </Text>
+    </Column>
+    <Column six wide justified>
+        <Text ui large >
+            Understanding the potential of a new medium can be challenging. With Imersia, your Mixed Reality experts can engage early, guiding ideation, exploring possibilities, and focusing on the best ways to connect with your customers.
+        </Text>
+    </Column>
+    <Column five wide justified>
+        <Text ui large style="text-align: justify">
             Deliver location-aware, interactive experiences via mobile WebApps or Mixed Reality Apps, reaching your audience through the device they use most.
         </Text>
     </Column>
