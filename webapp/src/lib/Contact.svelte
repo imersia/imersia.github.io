8a715164e5988267b0dd7a2bc762f92663885bac
Roy C. Davies <roycdavies@gmail.com>
2025-01-15 07:49:56 +1300

    Tweaks

--- webapp/src/lib/Contact.svelte
+++ webapp/src/lib/Contact.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
-    import { Container, Table, Table_Row, Table_Body, Table_Col, Link, Icon, Column, Text } from "svelte-fomantic-ui";
+    import { Container, Table, Table_Row, Table_Body, Table_Col, Link, Icon, Column, Text, Divider, Image } from "svelte-fomantic-ui";
+    import contact from "../assets/contact.jpg";
 
     export let page = "start";
 </script>
 
-<Container ui style="padding-top:50px;min-height:200px;padding-bottom:50px;">
+<Container ui grid style="padding-top:50px;min-height:200px;padding-bottom:50px;">
     <Column sixteen wide centered>
         <Text ui big>Contact</Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Lead Developer <Icon ui code/></Table_Col>
-                <Table_Col><Link href="mailto:roy@imersia.com?subject=Making contact">Dr. Roy C. Davies</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Lead Strategist <Icon ui running/></Table_Col>
-                <Table_Col><Link href="mailto:jon@imersia.com?subject=Making contact">Jon Lowther</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Divider ui/>
+    </Column>
+    <Column sixteen wide justified>
+        <Image ui src={contact}/>
+    </Column>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Lead Developer <Icon ui code/></Table_Col>
+                    <Table_Col><Link href="mailto:roy@imersia.com?subject=Making contact">Dr. Roy C. Davies</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Lead Strategist <Icon ui running/></Table_Col>
+                    <Table_Col><Link href="mailto:jon@imersia.com?subject=Making contact">Jon Lowther</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
 </Container>
--- webapp/src/lib/Resources.svelte
+++ webapp/src/lib/Resources.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { Container, Header, Table, Table_Row, Table_Body, Table_Col, Link, Icon, Column, Text, Divider } from "svelte-fomantic-ui";
+    import { Container, Image, Table, Table_Row, Table_Body, Table_Col, Link, Icon, Column, Text, Divider } from "svelte-fomantic-ui";
+    import resources from "../assets/resources.jpg";
 
     export let page = "start";
 
@@ -17,43 +18,49 @@
         </Text>
     </Column>
     <Column sixteen wide justified>
-        <Divider ui/>
+        <Image ui src={resources}/>
+    </Column>
+    <Column sixteen wide justified>
         <Text ui large>
             Client Apps can be built using your favorite tool.  Our interactive GeoSpatail gaming Apps (Android and iOS) use <Link href="https://www.unity.com" target="_blank">Unity</Link>, whilst our mobile WebApps and Dashboards use <Link href="https://react.dev/" target="_blank">React</Link> (though now we would receommend <Link href="https://svelte.dev/" target="_blank">Svelte</Link>).  All modern App and WebApp languages can work with <Link href="https://restfulapi.net/" target="_blank">RESTful APIs</Link> and most can work with <Link href="https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API" target="_blank">Websockets</Link> such as with Imersia.  Even small devices like <Link href="https://www.arduino.cc/" target="_blank">Arduino Developer Boards</Link> can send data from sensors to an Imersia Mixed Reality Server.
         </Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Documentation&nbsp;<Icon ui file/></Table_Col>
-                <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/wiki/Home" target="_blank">https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/wiki/Home</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Release Version&nbsp;<Icon ui running/></Table_Col>
-                <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/" target="_blank">https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>RESTful API&nbsp;<Icon ui code branch/></Table_Col>
-                <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/STARTHERE/Documentation/API%20COMMANDS.md" target="_blank">.../STARTHERE/Documentation/API%20COMMANDS.md</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Websockets&nbsp;<Icon ui plug/></Table_Col>
-                <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/STARTHERE/Documentation/WEBSOCKETS.md" target="_blank">.../STARTHERE/Documentation/WEBSOCKETS.md</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Documentation&nbsp;<Icon ui file/></Table_Col>
+                    <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/wiki/Home" target="_blank">https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/wiki/Home</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Release Version&nbsp;<Icon ui running/></Table_Col>
+                    <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/" target="_blank">https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>RESTful API&nbsp;<Icon ui code branch/></Table_Col>
+                    <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/STARTHERE/Documentation/API%20COMMANDS.md" target="_blank">.../STARTHERE/Documentation/API%20COMMANDS.md</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Websockets&nbsp;<Icon ui plug/></Table_Col>
+                    <Table_Col><Link href="https://bitbucket.org/imersiageneral/cloud-mixed-reality-server-erlang-rel/src/master/STARTHERE/Documentation/WEBSOCKETS.md" target="_blank">.../STARTHERE/Documentation/WEBSOCKETS.md</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
     <Column sixteen wide justified>
         <Divider ui/>
         <Text ui large>
             If you are curious about how this website was created, it is a static site created using Svelte, with the graphics library Fomantic UI using the vite framework.  It is hosted on github.io.
         </Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Imersia Website <Icon ui globe/></Table_Col>
-                <Table_Col><Link href="https://github.com/imersia/imersia.github.io" target="_blank">https://github.com/imersia/imersia.github.io</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Imersia Website <Icon ui globe/></Table_Col>
+                    <Table_Col><Link href="https://github.com/imersia/imersia.github.io" target="_blank">https://github.com/imersia/imersia.github.io</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
 </Container>
